da7b1b4f1b17e6da1d94dc5d823d75dce720facf
Gharbi Adlen <adlen025@gmail.com>
2015-05-08 12:35:42 +0100

    Initializing

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+var types = ["alter", "create", "drop", "rename", "truncate"];
+
+//TODO add the syntax of every statement and its variants
+
+//TODO identify string statement types
+
+//TODO apply statement type to the string
+
+//TODO parse the string
+
+//TODO repeat the process for all the strings (ending with a semicolon)
+
+module.exports = {
+
+};
