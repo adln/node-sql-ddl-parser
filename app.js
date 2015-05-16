16f8bede69065edec43ffd18e618dcc9da9cd983
Gharbi Adlen <adlen025@gmail.com>
2015-05-16 19:18:47 +0100

    Added the Table Information Object Schema

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var SqlParser = require('./index'),
+var SqlParser = require('./lib/index'),
 	Parser = new SqlParser(),
 	query = "CREATE TABLE `City` (`ID` int(11) NOT NULL AUTO_INCREMENT,`Name` char(35) NOT NULL DEFAULT '',`CountryCode` char(3) NOT NULL DEFAULT '',`District` char(20) NOT NULL DEFAULT '',`Population` int(11) NOT NULL DEFAULT '0', PRIMARY KEY (`ID`)) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=latin1;";
 
 Parser.setQuery(query);
-console.log(Parser.getInformations());
-
-Parser.setQuery('CREATE TABLE');
-console.log(Parser.getInformations());
+console.log('####################\n##### QUERY ########\n####################\n');
+console.log(Parser.getQuery());
+console.log('####################\n##### TABLE ########\n####################\n');
+console.log(Parser.getTableInformations());
+//console.log(Parser.getTableInformations());
--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 /// <reference path="../typings/mocha/mocha.d.ts"/>
+require('../lib/helper');
 var should = require('chai').should(),
 	expect = require('chai').expect(),
-	SqlParser = require('../index'),
+	SqlParser = require('../lib/index'),
 	Parser = new SqlParser(),
 	query = "CREATE TABLE `City` (`ID` int(11) NOT NULL AUTO_INCREMENT,`Name` char(35) NOT NULL DEFAULT '',`CountryCode` char(3) NOT NULL DEFAULT '',`District` char(20) NOT NULL DEFAULT '',`Population` int(11) NOT NULL DEFAULT '0', PRIMARY KEY (`ID`)) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=latin1;";
 
@@ -15,24 +16,33 @@ describe('Parser', function () {
 		});
 	});
 
-	describe('#getInformations()', function () {
+	describe('#getTableInformations()', function () {
 		it('Should return a string', function () {
-			Parser.setQuery(query).getInformations().table_name.should.be.a('string');
+			Parser.setQuery(query).getTableInformations().table_name.should.be.a('string');
 		});
-		it('Should return an array of columns', function () {
-			Parser.setQuery(query).getInformations().columns.should.be.a('array');
-		});
+		
 		it('Should return an object', function () {
-			Parser.setQuery(query).getInformations().should.be.a('object');
+			Parser.setQuery(query).getTableInformations().should.be.a('object');
 		});
 		it('Should not return an empty table name', function () {
-			Parser.setQuery(query).getInformations().should.have.property('table_name');
-			Parser.setQuery(query).getInformations().table_name.should.not.be.a('undefined');
+			Parser.setQuery(query).getTableInformations().should.have.property('table_name');
+			Parser.setQuery(query).getTableInformations().table_name.should.not.be.a('undefined');
 		});
 		
 		it('Should return false if there is no table name', function () {
-			Parser.setQuery('CREATE SOMETHING').getInformations().should.be.a('boolean');
-			Parser.setQuery('CREATE SOMETHING').getInformations().should.equal(false);			
+			Parser.setQuery('CREATE SOMETHING').getTableInformations().should.be.a('boolean');
+			Parser.setQuery('CREATE SOMETHING').getTableInformations().should.equal(false);			
+		});	
+					
+		it('Should return an object', function () {			
+			Parser.setQuery(query).getTableInformations().definition.should.be.a('object');
 		});
-	});
+		it('Should be composed of two attributes', function () {
+			
+			Parser.setQuery(query).getTableInformations().definition.size().should.equal(2);
+		});
+		it('Should return an array of columns', function () {
+			Parser.setQuery(query).getTableInformations().definition.columns.should.be.a('array');
+		});
+	});		
 });
